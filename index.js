1a803a084e7ab77191f4be11e443621c2ef3342c
Buddhika Shahan <100762985+buddhikashahan@users.noreply.github.com>
2024-01-23 11:36:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+const {
+    default: makeWASocket,
+    useMultiFileAuthState,
+    DisconnectReason,
+    jidNormalizedUser,
+    getContentType
+} = require('@adiwajshing/baileys')
+
+async function connectToWA() {
+    const { state, saveCreds } = await useMultiFileAuthState('./auth_info_baileys')
+    const conn = makeWASocket({
+        logger: P({ level: 'silent' }),
+        printQRInTerminal: true,
+        generateHighQualityLinkPreview: true,
+        auth: state,
+    })
+
+    conn.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect } = update
+        if (connection === 'close') {
+            if (lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut) {
+                connectToWA()
+            }
+        } else if (connection === 'open') {
+            console.log('Bot Connected ✅')
+        }
+    })
+    conn.ev.on('creds.update', saveCreds)
+    conn.ev.on('messages.upsert', async (mek) => {
+        try {
+
+            mek = mek.messages[0]
+            if (!mek.message) return
+            mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+            if (mek.key && mek.key.remoteJid === 'status@broadcast') return
+            const type = getContentType(mek.message)
+			const content = JSON.stringify(mek.message)
+			const from = mek.key.remoteJid
+			
+			const quoted = type == 'extendedTextMessage' && mek.message.extendedTextMessage.contextInfo != null ? mek.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
+			const body = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : ( type == 'listResponseMessage') && mek.message.listResponseMessage.singleSelectReply.selectedRowId? mek.message.listResponseMessage.singleSelectReply.selectedRowId : (type == 'buttonsResponseMessage') && mek.message.buttonsResponseMessage.selectedButtonId  ? mek.message.buttonsResponseMessage.selectedButtonId  : (type == "templateButtonReplyMessage") && mek.message.templateButtonReplyMessage.selectedId ? mek.message.templateButtonReplyMessage.selectedId  :  (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : ''
+			
+			const isCmd = body.startsWith(prefix)
+			const command = isCmd ? body.slice(prefix.length).trim().split(' ').shift().toLowerCase() : ''
+			
+			const args = body.trim().split(/ +/).slice(1)
+			const q = args.join(' ')
+			const isGroup = from.endsWith('@g.us')
+			const sender = mek.key.fromMe ? (conn.user.id.split(':')[0]+'@s.whatsapp.net' || conn.user.id) : (mek.key.participant || mek.key.remoteJid)
+			const senderNumber = sender.split('@')[0]
+			const botNumber = conn.user.id.split(':')[0]
+			const pushname = mek.pushName || 'Sin Nombre'
+			
+			
+			const isMe = botNumber.includes(senderNumber)
+			const isOwner = owner.includes(senderNumber) || isMe
+			
+			const reply = (teks) => {
+				conn.sendMessage(from, { text: teks }, { quoted: mek })
+			}
+            const groupMetadata = mek.isGroup ? await conn.groupMetadata(mek.chat).catch(e => {}) : ''
+            const groupName = mek.isGroup ? groupMetadata.subject : ''
+            const participants = mek.isGroup ? await groupMetadata.participants : ''
+            const groupAdmins = mek.isGroup ? await participants.filter(v => v.admin !== null).map(v => v.id) : ''
+            const groupOwner = mek.isGroup ? groupMetadata.owner : ''
+    	    const isBotAdmins = mek.isGroup ? groupAdmins.includes(botNumber) : false
+    	    const isAdmins = mek.isGroup ? groupAdmins.includes(mek.sender) : false
+
+            
+            switch (command) {
+                case 'jid':
+                    reply(from)
+                    break
+
+                default:
+                    if (isOwner && body.startsWith('>')) {
+                        try {
+                            await reply(util.format(await eval(`(async () => {${body.slice(1)}})()`)))
+                        } catch(e) {
+                            await reply(util.format(e))
+                        }
+                    }
+            }
+        } catch (e) {
+            const isError = String(e)
+            console.log(isError)
+        }
+    })
+}
+
+    connectToWA()
